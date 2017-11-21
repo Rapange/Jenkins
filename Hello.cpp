1aa4df92759801a6ab9ba08f93cee3bd8fed7a33
Rapange <h_ero@hotmail.es>
2017-11-21 16:56:12 -0500

    Update and rename Hello.py to Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+double myDivision(double a, double b){
+  if(b == 0) return 0;
+  return a / b;
+}
+
+int main(int argc, char **argv){
+  double a = stoi(argv[1]);
+  double b = stoi(argv[2]);
+  cout<<myDivision(a,b)<<endl;
+  return 0;
+}
